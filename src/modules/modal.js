5c27e433d1da9490a90d768d222f8e7b9f8789d0
Sergey <segaoff@icloud.com>
2022-11-27 21:02:11 +0300

    пофиксил отображения модального окна

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,32 +1,42 @@
 const modal = () => {
-    const modalOverlay = document.querySelector('.modal__overlay')
-    const modalCallback = document.querySelector('.modal__callback')
-    const modalOverlayDelivery = document.querySelector('.modal__overlay-delivery')
-    const modalCallbackDelivery = document.querySelector('.modal__callback-delivery')
+    const modalOverlay = document.querySelector('.modal__overlay');
+    const modalCallback = document.querySelector('.modal__callback');
+    const modalOverlayDelivery = document.querySelector('.modal__overlay-delivery');
+    const modalCallbackDelivery = document.querySelector('.modal__callback-delivery');
+    const modalMainWrapper = document.querySelectorAll('.modal__main-wrapper');
 
     const toggleModalPayment = (e) => {
         if (e.target.closest('.paymet__method-btn') || e.target.closest('.total__payment-btn-svg')) {
             e.preventDefault();
-            modalCallback.style.display = 'block'
-            modalOverlay.style.display = 'block'
+            modalCallback.style.display = 'block';
+            modalOverlay.style.display = 'block';
         }
         if (e.target.closest('.modal__close-svg') || e.target.closest('.modal__overlay') || e.target.closest('.modal__footer-btn')) {
-            modalCallback.style.display = 'none'
-            modalOverlay.style.display = 'none'
+            modalCallback.style.display = 'none';
+            modalOverlay.style.display = 'none';
         }
     }
     const toggleModalDelivery = (e) => {
         if (e.target.closest('.delivery__method-btn') || e.target.closest('.total__delivery-btn-svg')) {
             e.preventDefault();
-            modalCallbackDelivery.style.display = 'block'
-            modalOverlayDelivery.style.display = 'block'
+            modalCallbackDelivery.style.display = 'block';
+            modalOverlayDelivery.style.display = 'block';
         }
         if (e.target.closest('.modal__close-svg') || e.target.closest('.modal__overlay-delivery') || e.target.closest('.modal__footer-btn')) {
-            modalCallbackDelivery.style.display = 'none'
-            modalOverlayDelivery.style.display = 'none'
+            modalCallbackDelivery.style.display = 'none';
+            modalOverlayDelivery.style.display = 'none';
         }
     }
     document.addEventListener("click", (e) => toggleModalPayment(e));
     document.addEventListener("click", (e) => toggleModalDelivery(e));
+
+    modalMainWrapper.forEach(item => {
+        if (item.lastElementChild.matches('.delivery__form-label')) {
+            item.lastElementChild.lastElementChild.addEventListener('click', () => {
+                item.remove();
+            })
+        }
+    })
+
 }
 export default modal
